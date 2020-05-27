35d0b468ba72e4023cf58381ff64e69cbe1e4b1b
Jan Macku <jamacku@redhat.com>
2020-05-27 12:16:23 +0200

    Simplify code to use only md5

--- hash-file.h
+++ hash-file.h
@@ -1,104 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <openssl/md2.h>
-#include <openssl/md4.h>
 #include <openssl/md5.h>
-#include <openssl/ripemd.h>
-#include <openssl/sha.h>
 
 #define BUFF_SIZE 1024
 
-enum hash_algo {
-  HASH_MD2,
-  HASH_MD4,
-  HASH_MD5,
-  HASH_RIPEMD160,
-  HASH_SHA1,
-  HASH_SHA224,
-  HASH_SHA256,
-  HASH_SHA384,
-  HASH_SHA512,
-  _HASH_MAX_ALGO
-};
-
-struct hash_opts {
-  char algo_name[16];
-  size_t hash_size;
-  size_t context_size;
-  int (*algo_init)(void*);
-  int (*algo_update)(void*, const unsigned char*, unsigned long);
-  int (*algo_final)(unsigned char*, void*);
-};
-
-struct hash_opts algos[_HASH_MAX_ALGO] = {{         /*HASH_MD2*/
-                                            "md2",
-                                            MD2_DIGEST_LENGTH,
-                                            sizeof(MD2_CTX),
-                                            &MD2_Init,
-                                            &MD2_Update,
-                                            &MD2_Final
-                                          }, {      /*HASH_MD4*/
-                                            "md4",
-                                            MD4_DIGEST_LENGTH,
-                                            sizeof(MD4_CTX),
-                                            &MD4_Init,
-                                            &MD4_Update,
-                                            &MD4_Final
-                                          }, {      /*HASH_MD5*/
-                                            "md5",
-                                            MD5_DIGEST_LENGTH,
-                                            sizeof(MD5_CTX),
-                                            &MD5_Init,
-                                            &MD5_Update,
-                                            &MD5_Final
-                                          }, {      /*HASH_RIPEMD160*/
-                                            "ripemd160",
-                                            RIPEMD160_DIGEST_LENGTH,
-                                            sizeof(RIPEMD160_CTX),
-                                            &RIPEMD160_Init,
-                                            &RIPEMD160_Update,
-                                            &RIPEMD160_Final
-                                          }, {      /*HASH_SHA1*/
-                                            "sha1",
-                                            SHA_DIGEST_LENGTH,
-                                            sizeof(SHA_CTX),
-                                            &SHA1_Init,
-                                            &SHA1_Update,
-                                            &SHA1_Final
-                                          }, {      /*HASH_SHA224*/
-                                            "sha224",
-                                            SHA224_DIGEST_LENGTH,
-                                            sizeof(SHA256_CTX),
-                                            &SHA224_Init,
-                                            &SHA224_Update,
-                                            &SHA224_Final
-                                          }, {      /*HASH_SHA256*/
-                                            "sha256",
-                                            SHA256_DIGEST_LENGTH,
-                                            sizeof(SHA256_CTX),
-                                            &SHA256_Init,
-                                            &SHA256_Update,
-                                            &SHA256_Final
-                                          }, {      /*HASH_SHA384*/
-                                            "sha384",
-                                            SHA384_DIGEST_LENGTH,
-                                            sizeof(SHA512_CTX),
-                                            &SHA384_Init,
-                                            &SHA384_Update,
-                                            &SHA384_Final
-                                          }, {      /*HASH_SHA512*/
-                                            "sha512",
-                                            SHA512_DIGEST_LENGTH,
-                                            sizeof(SHA512_CTX),
-                                            &SHA512_Init,
-                                            &SHA512_Update,
-                                            &SHA512_Final
-                                          }};
-
-void* hash_file (char* file_path, unsigned char* digestbuf, struct hash_opts* algo)
+void* hash_file_md5 (char* file_path, unsigned char* digestbuf)
 {
-  void* contxt = malloc(algo->context_size);
+  void* contxt = malloc(sizeof(MD5_CTX));
   int bytes;
   unsigned char buff[BUFF_SIZE];
   FILE *fd = fopen(file_path, "rb");
@@ -108,13 +17,13 @@ void* hash_file (char* file_path, unsigned char* digestbuf, struct hash_opts* al
     return (void*)3;
   }
   
-  algo->algo_init(contxt);
+  MD5_Init(contxt);
 
   while ((bytes = fread(buff, 1, BUFF_SIZE, fd)) != 0) {
-    algo->algo_update(contxt, buff, bytes);
+    MD5_Update(contxt, buff, bytes);
   }
   
-  algo->algo_final(digestbuf, contxt);
+  MD5_Final(digestbuf, contxt);
     
   fclose(fd);
   free(contxt);
--- main.c
+++ main.c
@@ -6,52 +6,20 @@
 
 int main (int argc, char** argv)
 {
-  int aflag = 0;
   int fflag = 0;
-  char *avalue = NULL;
   char *fvalue = NULL;
-  unsigned algo_index;
   int c; 
   opterr = 0;
 
-  while ((c = getopt (argc, argv, "a:f:")) != -1) {
+  while ((c = getopt (argc, argv, "f:")) != -1) {
     switch (c) {
-      case 'a':
-        aflag = 1;
-        avalue = optarg;
-        if ((strcmp(avalue, "md2")) == 0) {
-          algo_index = HASH_MD2;
-        } else if ((strcmp(avalue, "md4")) == 0) {
-          algo_index = HASH_MD4;
-        } else if ((strcmp(avalue, "md5")) == 0) {
-          algo_index = HASH_MD5;
-        } else if ((strcmp(avalue, "ripemd160")) == 0) {
-          algo_index = HASH_RIPEMD160;
-        } else if ((strcmp(avalue, "sha1")) == 0) {
-          algo_index = HASH_SHA1;
-        } else if ((strcmp(avalue, "sha224")) == 0) {
-          algo_index = HASH_SHA224;
-        } else if ((strcmp(avalue, "sha256")) == 0) {
-          algo_index = HASH_SHA256;
-        } else if ((strcmp(avalue, "sha384")) == 0) {
-          algo_index = HASH_SHA384;
-        } else if ((strcmp(avalue, "sha512")) == 0) {
-          algo_index = HASH_SHA512;
-        } else {
-          printf("Missing parameter a!!\n");
-          return 5;
-        }
-        break;
-
       case 'f':
         fflag = 1;
         fvalue = optarg;
         break;
        
       case '?':
-        if (optopt == 'a') {
-          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-        } else if (optopt == 'f') { 
+        if (optopt == 'f') { 
           fprintf (stderr, "Option -%c requires an argument.\n", optopt);
         } else if (isprint(optopt)) {
           fprintf (stderr, "Unknown option `-%c'.\n", optopt);
@@ -65,12 +33,12 @@ int main (int argc, char** argv)
     }
   }
 
-  if((aflag == 1) && (fflag == 1)) {
-    unsigned char* digestbuf = (unsigned char*)malloc(algos[algo_index].hash_size); 
+  if(fflag == 1) {
+    unsigned char* digestbuf = (unsigned char*)malloc(MD5_DIGEST_LENGTH); 
     char* file_name = fvalue;
 
-    hash_file(file_name, digestbuf, &algos[algo_index]);
-    for (size_t i = 0; i < algos[algo_index].hash_size; i++) {
+    hash_file_md5(file_name, digestbuf);
+    for (size_t i = 0; i < MD5_DIGEST_LENGTH; i++) {
       printf("%02x", digestbuf[i]);
     }
     printf(" %s\n", fvalue);
